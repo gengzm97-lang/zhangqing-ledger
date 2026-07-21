4a16bf96583d96bec6ad631b975ed1d4b0dbca5b
geng <gengzm97@gmail.com>
2026-07-21 02:07:09 -0300

    Configure Supabase cloud sync

--- cloud-config.js
+++ cloud-config.js
@@ -1,6 +1,6 @@
 // 部署前可直接填写以下两个公开配置；也可以在软件“数据管理”页面中填写。
 // 注意：只能填写 Publishable key 或 anon key，绝不能填写 service_role key。
 window.ZHANGQING_CLOUD = {
-  supabaseUrl: "",
-  supabasePublishableKey: ""
+  supabaseUrl: "https://bafdcllbctujdwfkicta.supabase.co",
+  supabasePublishableKey: "sb_publishable_SGJDdU7jl5wOyqr8e1sztQ_mT92IOLC"
 };
